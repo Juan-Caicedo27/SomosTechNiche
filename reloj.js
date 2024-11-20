484d6d25f32063b3bdb50d826a9060a5169eab65
Juan-Caicedo27 <juan.caicedoc@uniagustiniana.edu.co>
2024-11-20 00:50:37 -0500

    Update reloj.js

--- reloj.js
+++ reloj.js
@@ -20,3 +20,40 @@ setInterval(updateTime, 1000);
 
 // mostrar la hora al cargar la página
 updateTime();
+
+//slider//
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.querySelectorAll(".slider");
+    let quadrates = document.querySelectorAll(".quadrate"); // Comillas corregidas
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // Corregido 'stile' a 'style'
+    }
+
+    for (i = 0; i < quadrates.length; i++) { // Corrección en el índice
+        quadrates[i].className = quadrates[i].className.replace(" active", ""); // Agregar espacio en " active"
+    }
+
+    slides[slideIndex - 1].style.display = "block"; // Corregido 'stile' a 'style'
+    quadrates[slideIndex - 1].className += " active";
+}
